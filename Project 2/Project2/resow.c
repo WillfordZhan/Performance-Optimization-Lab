ccd65a4ac13533fa374e8bae24655596eb510485
willford <willfordzhan@gmail.com>
2019-10-26 09:15:17 +0200

    Fix: fixed the bug with args and create_dataset

--- Project 2/Project2/resow.c
+++ Project 2/Project2/resow.c
@@ -5,7 +5,6 @@
 
 float get_Random(float rmax){
     float x = ((float)rand()/(float)RAND_MAX)*rmax;
-    // printf("%f\n", x);
     return x;
 }
 
@@ -14,99 +13,57 @@ void create_dataset(int dataSize, char* filename){
     srand((unsigned int)time(NULL));
 
     FILE *fp;
-    float data[dataSize];
-    fp = fopen(filename,"w");
+    float *data = malloc(sizeof(float) * dataSize);
+    fp = fopen(filename,"wb");
+    
     for (int i = 0; i < dataSize; i++)
     {
         data[i] = get_Random(100);
     }
-    for (int i = 0; i < dataSize; i++)
-    {
-        printf("%f\n",data[i]);
-    }
-    
+    free(data);
     fwrite(data, sizeof(float), sizeof(dataSize), fp);
     fclose(fp);
 }
 
-float* load_dataSet(int dataSize, char *filename){
+void load_dataSet(float *dataSet,int dataSize, int bufferSize, char *filename){
     FILE *fp;
-    long fSize;
-    
-    fp = fopen(filename,"r");
-    // dstArray settings, get the size of the file
-    
-    float dstArray[dataSize];
-    fread(dstArray, sizeof(float), dataSize, fp);
-
-    fclose(fp);
-    return dstArray;
-}
-
-void swap(float *xp, float *yp){
-    float temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-
-void sort_dataSet(float arr[], int n){
-    int i, j, min_idx;
-    // One by one move boundary of unsorted subarray
-    for (i = 0; i < n-1; i++){
-    // Find the minimum element in unsorted array
-        min_idx = i;
-        for (j = i+1; j < n; j++)
-            if (arr[j] < arr[min_idx])
-                min_idx = j;
-
-        // Swap the found minimum element with the first element
-        swap(&arr[min_idx], &arr[i]);
+    fp = fopen(filename,"rb");
+    for (int i = 0; i < dataSize; i+= bufferSize)
+    {
+        fread(dataSet+i, sizeof(float), bufferSize, fp);
     }
-}
-
-void write_dataSet(int dataSize, char *filename){
-    FILE *fp;
-    long fSize;
-    
-    fp = fopen(filename,"wb");
-    // dstArray settings, get the size of the file
-    
-    float *dstArray;
-    fseek(fp, 0, SEEK_END);
-    fSize = ftell(fp);
-    rewind(fp);
-
-    dstArray = (float *) malloc (fSize * sizeof(float));
-    fwrite(dstArray, sizeof(float), dataSize, fp);
     fclose(fp);
-    free(dstArray);
 }
 
-
-void main(int argc, char **argv){
+int main(int argc, char **argv){
     int bufferSize, dataSize;
     char *filename;
+    float *dataSet;
 
     if (argc != 4)
     {
-        printf("wrong usage!");
+        printf("Usage: resow.c <dataSize> <bufferSize> <filename>\n");
         exit(1);
     }
     
-    dataSize = atoi(argv[0]);
-    bufferSize = atoi(argv[1]);
-    filename = argv[2];
+    dataSize = atoi(argv[1]);
+    bufferSize = atoi(argv[2]);
+    filename = argv[3];
 
+    dataSet = malloc(sizeof(float)* dataSize);
 
-    create_dataset(100, "dataSet.txt");
-    printf("-------------");
-    float *dataPtr = load_dataSet(100, "dataSet");
-
-    printf("-------------");
-
-
-    // sort_dataSet(dataPtr, 100);
-    // printf("-------------");
-    // print_dataSet(100,"dataSet");
+    // printf("creating dataset\n");
+    // create_dataset(dataSize, filename);
+    // printf("-------------Creation Over-------------\n");
+    
+    printf("loading dataset\n");
+    load_dataSet(dataSet,dataSize,bufferSize,filename);
+    printf("-------------Loading Over-------------\n");
+    for (int i = 0; i < dataSize; i++)
+    {
+        printf("%f\n",*(dataSet+i));
+    }
+    free(dataSet);
+    return 0;
 }
 
