20094a509011df084caf8bb75f944bd2ca900b48
willford <willfordzhan@gmail.com>
2019-10-26 09:30:03 +0200

    Fix: fixed the output logic of create_dataset

--- Project 2/Project2/resow.c
+++ Project 2/Project2/resow.c
@@ -19,46 +19,54 @@ void create_dataset(int dataSize, char* filename){
     for (int i = 0; i < dataSize; i++)
     {
         data[i] = get_Random(100);
+        printf("%f\n",data[i]);
     }
+    fwrite(data, sizeof(float), dataSize, fp);
     free(data);
-    fwrite(data, sizeof(float), sizeof(dataSize), fp);
     fclose(fp);
 }
 
-void load_dataSet(float *dataSet,int dataSize, int bufferSize, char *filename){
+void load_dataSet(float *dataSet,int dataSize, int recordSize, char *filename){
     FILE *fp;
     fp = fopen(filename,"rb");
-    for (int i = 0; i < dataSize; i+= bufferSize)
+    for (int i = 0; i < dataSize; i+= recordSize)
     {
-        fread(dataSet+i, sizeof(float), bufferSize, fp);
+        if (recordSize > dataSize - i)
+        {
+            recordSize = dataSize - i;
+        }
+        fread(dataSet+i, sizeof(float), recordSize, fp);
     }
     fclose(fp);
 }
 
 int main(int argc, char **argv){
-    int bufferSize, dataSize;
+    int recordSize, dataSize;
     char *filename;
     float *dataSet;
 
     if (argc != 4)
     {
-        printf("Usage: resow.c <dataSize> <bufferSize> <filename>\n");
+        printf("Usage: resow.c <dataSize> <recordSize> <filename>\n");
         exit(1);
     }
     
     dataSize = atoi(argv[1]);
-    bufferSize = atoi(argv[2]);
+    recordSize = atoi(argv[2]);
     filename = argv[3];
 
     dataSet = malloc(sizeof(float)* dataSize);
 
-    // printf("creating dataset\n");
-    // create_dataset(dataSize, filename);
-    // printf("-------------Creation Over-------------\n");
+    printf("creating dataset\n");
+    create_dataset(dataSize, filename);
+    printf("-------------Creation Over-------------\n");
     
+    return 1;
     printf("loading dataset\n");
-    load_dataSet(dataSet,dataSize,bufferSize,filename);
+    load_dataSet(dataSet,dataSize,recordSize,filename);
     printf("-------------Loading Over-------------\n");
+    printf("%f\n",*dataSet);
+    return 1;
     for (int i = 0; i < dataSize; i++)
     {
         printf("%f\n",*(dataSet+i));
